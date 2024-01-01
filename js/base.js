ad3dbb9e6acc93a26ae6180c205d6d7624201724
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-02 00:49:22 +0100

    Added the ability to dynamically load articles and added active2

--- js/base.js
+++ js/base.js
@@ -8,3 +8,13 @@ function cambiaTab(elemento) {
     // Aggiungi la classe "active" all'elemento cliccato
     elemento.classList.add('active');
 } 
+function cambiaTab2(elemento) {
+  // Rimuovi la classe "active" dall'elemento corrente
+  let elementiAttivi = document.querySelectorAll('a.active2');
+  elementiAttivi.forEach(function (el) {
+    el.classList.remove('active2');
+  });
+
+  // Aggiungi la classe "active" all'elemento cliccato
+  elemento.classList.add('active2');
+} 

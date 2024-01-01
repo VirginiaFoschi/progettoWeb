e80bfda8c847e45068a12aee4ed27d6506a6fffd
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-02 00:48:22 +0100

    Add type of post loaded dynamically

---
+++ js/profile-post.js
@@ -0,0 +1,10 @@
+function caricaArticoli(file) {
+    // Usa la funzione fetch per ottenere il contenuto del file
+    fetch(file)
+      .then(response => response.text())  // Ottieni il testo della risposta
+      .then(data => {
+        // Aggiorna il contenuto del div "contenitore-post" con il testo ottenuto
+        document.getElementById('contenitore-post').innerHTML = data;
+      })
+      .catch(error => console.error('Errore nel caricamento degli articoli:', error));
+  }

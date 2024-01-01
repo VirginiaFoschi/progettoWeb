16e286e6e92c4d2413a7186c63c1577d0f1c03b4
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-01 14:37:47 +0100

    file for login

---
+++ js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('logbtn').addEventListener('click', function() {
+    window.location.href = "login.html";
+})
+
+document.getElementById('signbtn').addEventListener('click', function() {
+    window.location.href = "sign.html";
+})

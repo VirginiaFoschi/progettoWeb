249f5ed117fd15588bf7b7fa7118e42ebddf272f
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-02 15:49:45 +0100

    add image to sign.html and delete button impostazioni from bacheca.html

---
+++ js/sign.js
@@ -0,0 +1,16 @@
+document.getElementById("input").addEventListener("change", function(event) {
+    let input = event.target;
+    let immagine = document.getElementById('profilo');
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          if (immagine) {
+            immagine.src = e.target.result;
+          } 
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+});

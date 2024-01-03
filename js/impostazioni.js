26bea5d017be4275f6e7e32c965d082cc7e8f829
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-03 01:02:16 +0100

    Add impostazioni, impostazioni-immagine, impostazione-indirizzo

---
+++ js/impostazioni.js
@@ -0,0 +1,15 @@
+document.getElementById('indietro').addEventListener('click', function() {
+    window.location.href = "profile.html";
+})
+
+document.getElementById('log-out').addEventListener('click', function() {
+    window.location.href = "home.html";
+})
+
+document.getElementById('cambia-img').addEventListener('click', function() {
+    window.location.href = "impostazioni-immagine.html";
+})
+
+document.getElementById('cambia-indirizzo').addEventListener('click', function() {
+    window.location.href = "impostazioni-indirizzo.html";
+})
--- js/profile-post.js
+++ js/profile-post.js
@@ -1,10 +1,15 @@
 function caricaArticoli(file) {
-    // Usa la funzione fetch per ottenere il contenuto del file
-    fetch(file)
-      .then(response => response.text())  // Ottieni il testo della risposta
-      .then(data => {
-        // Aggiorna il contenuto del div "contenitore-post" con il testo ottenuto
-        document.getElementById('contenitore-post').innerHTML = data;
-      })
-      .catch(error => console.error('Errore nel caricamento degli articoli:', error));
-  }
+  // Usa la funzione fetch per ottenere il contenuto del file
+  fetch(file)
+    .then(response => response.text())  // Ottieni il testo della risposta
+    .then(data => {
+      // Aggiorna il contenuto del div "contenitore-post" con il testo ottenuto
+      document.getElementById('contenitore-post').innerHTML = data;
+    })
+    .catch(error => console.error('Errore nel caricamento degli articoli:', error));
+}
+
+
+document.getElementById('impostazioni').addEventListener('click', function () {
+  window.location.href = "impostazioni.html";
+})

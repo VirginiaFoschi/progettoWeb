c2564bfb00c27a767075544ba2b24514f58d95a6
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-08 10:46:54 +0100

    fixed impostazioni

--- js/impostazioni.js
+++ js/impostazioni.js
@@ -1,19 +1,23 @@
-document.getElementById('indietro').addEventListener('click', function() {
-    window.location.href = "profilo.html";
-})
+function backImp() {
+    window.location.href = "impostazioni.html"
+}
 
-document.getElementById('log-out').addEventListener('click', function() {
-    window.location.href = "home.html";
-})
+function logout() {
+    window.location.href = "home.html"
+}
 
-document.getElementById('cambia-img').addEventListener('click', function() {
-    window.location.href = "impostazioni-immagine.html";
-})
+function changeImg() {
+    window.location.href = "impostazioni-immagine.html"
+}
 
-document.getElementById('cambia-indirizzo').addEventListener('click', function() {
-    window.location.href = "impostazioni-indirizzo.html";
-})
+function backProfilo() {
+    window.location.href = "profilo.html"
+}
 
-document.getElementById('modifica-generi').addEventListener('click', function() {
-    window.location.href = "impostazioni-genere.html";
-})
+function changeInd() {
+    window.location.href = "impostazioni-indirizzo.html"
+}
+
+function changeGenere() {
+    window.location.href = "impostazioni-genere.html"
+}

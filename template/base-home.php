00e9d3b91b4615d90ffe06d196d647e8d75f558e
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-12 17:56:32 +0100

    Modified file profilo.html to profilo.php

--- template/base-home.php
+++ template/base-home.php
@@ -29,7 +29,7 @@
             <footer class="bg-white footer">
                 <ul class="nav nav-pills nav-fill">
                     <li class="nav-item">
-                        <a class="nav-link active" title="bacheca" href="bacheca.html" onclick="cambiaTab(this)">
+                        <a class="nav-link" title="bacheca" href="bacheca.html" onclick="cambiaTab(this)">
                             <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-clipboard icon" viewBox="0 0 16 16">
                                 <path
                                     d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
@@ -57,7 +57,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" title="profilo" href="profilo.html" onclick="cambiaTab(this)">
+                        <a class="nav-link active" title="profilo" href="profilo.php" onclick="cambiaTab(this)">
                             <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-person icon" viewBox="0 0 16 16">
                                 <path
                                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />

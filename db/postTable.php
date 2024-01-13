f0c364ce701fc354e26348480501c4d6a5cd9ace
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-13 15:09:22 +0100

    Add getAnnuncioProfilo num of like

--- db/postTable.php
+++ db/postTable.php
@@ -1,35 +1,59 @@
 <?php
 
 /*Classe che contiene i tre tipi di post: annuncio, recensione e post libro*/
-class PostTable {
+class PostTable
+{
     private $db;
 
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->db = $db;
     }
 
-    public function getAnnuncioProfilo($username) {
-        $stmt = $this->db->prepare("SELECT dataEvento, luogo, nomeEvento, descrizione, immagine FROM evento WHERE Usurname_Autore = ?"); 
+
+    public function getAnnuncioProfilo($username)
+    {
+        $stmt = $this->db->prepare("SELECT ID_Evento, dataEvento, luogo, nomeEvento, descrizione, immagine FROM evento WHERE Usurname_Autore = ?");
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC); 
+        $annunci = $result->fetch_all(MYSQLI_ASSOC);
+
+        $risultatoFinale = array();
+
+        foreach ($annunci as $annuncio) {
+
+            $stmt = $this->db->prepare("SELECT COUNT(*) AS numLike FROM interesse WHERE ID_Evento = ?");
+            $stmt->bind_param('i', $annuncio["ID_Evento"]);
+            $stmt->execute();
+            $result1 = $stmt->get_result();
+            $row = $result1->fetch_assoc();
+
+            $risultatoFinale[] = array(
+                'Evento' => $annuncio,
+                'NumLike' => $row['numLike']
+            );
+
+        }
+        return $risultatoFinale;
     }
 
-    public function getRecensioneProfilo($username) {
-        $stmt = $this->db->prepare("SELECT * FROM recensione WHERE Autore_Recensione = ? "); 
+    public function getRecensioneProfilo($username)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM recensione WHERE Autore_Recensione = ? ");
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC); 
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getPostLibroProfilo($username) {
-        $stmt = $this->db->prepare("SELECT Titolo, Autore, Casa_Editrice, Trama, Condizioni, Immagine, Nome_Genere FROM libro_postato WHERE Usurname_Autore = ? "); 
+    public function getPostLibroProfilo($username)
+    {
+        $stmt = $this->db->prepare("SELECT Titolo, Autore, Casa_Editrice, Trama, Condizioni, Immagine, Nome_Genere FROM libro_postato WHERE Usurname_Autore = ? ");
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC); 
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
 ?>
--- template/profilo-post.php
+++ template/profilo-post.php
@@ -1,15 +1,15 @@
 <div class="row justify-content-center content">
+    <?php foreach ($$templateparams["annuncio"] as $annuncio): ?>
     <!-- Annuncio -->
     <article class="article-annuncio bg-body border mb-3">
-
         <header class="row">
             <a href="#">
-                <img src="img/immagineProfilo.png" alt="Foto profilo" class="img-post">
-                Utente Autore
+                <img src="<?php UPLOAD_DIR.$annuncio["Evento"]["Immagine"]?>" alt="Foto profilo" class="img-post">
+                <?php echo $templateparams["username"]?>
             </a>
         </header>
         <section class="px-3 mb-4">
-            <h2>Nome evento</h2>
+            <h2><?php echo $annuncio["Evento"]["nomeEvento"]?></h2>
             <ul>
                 <li>
                     <span>
@@ -18,7 +18,7 @@
                             <path
                                 d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                         </svg>
-                        Data Evento
+                        <?php echo $annuncio["Evento"]["dataEvento"]?>
                     </span>
                 </li>
                 <li>
@@ -29,16 +29,11 @@
                                 d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
                             <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                         </svg>
-                        Luogo
+                        <?php echo $annuncio["Evento"]["luogo"]?>
                     </span>
                 </li>
                 <li>
-                    <p id="descrizione-annuncio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-                        aperiam,
-                        facilis iusto accusantium, veniam eos eligendi laboriosam magnam assumenda quibusdam placeat
-                        illum
-                        esse
-                        dolores. Ullam molestias exercitationem consequuntur enim a?</p>
+                    <p id="descrizione-annuncio"><?php echo $annuncio["Evento"]["descrizione"]?></p>
                 </li>
             </ul>
         </section>
@@ -47,77 +42,8 @@
                 viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
             </svg>
-            <p>Num like</p>
+            <p><?php echo $annuncio["NumLike"]["numLike"]?></p>
         </footer>
     </article>
-    <!--Recensione-->
-    <article class="article-recensione bg-body border mb-3">
-        <header class="row">
-            <a href="#">
-                <img src="img/immagineProfilo.png" alt="Foto profilo" class="img-post" />
-                Utente Autore
-            </a>
-        </header>
-        <section class="px-3 mb-4">
-            <ul>
-                <li>
-                    <div class="d-flex align-items-center">
-                        <img src="img/html5-js-css3.png" alt="Copertina libro" class="image" />
-                        <ul>
-                            <li>
-                                <h2>Titolo Libro</h2>
-                            </li>
-                            <li>
-                                <p>Autore del Libro</p>
-                            </li>
-                            <li>
-                                <p>Genere</p>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" class="bi bi-star-fill"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" class="bi bi-star-fill"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" class="bi bi-star-fill"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" class="bi bi-star-fill"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="grey" class="bi bi-star-fill"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                </li>
-                <li>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus assumenda porro, accusantium
-                        aliquid
-                        modi culpa quae corporis nesciunt, et soluta officiis fugiat ea minima repellat, quisquam
-                        expedita
-                        tempore ab mollitia.</p>
-                </li>
-            </ul>
-        </section>
-        <footer>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-heart-fill heart-icon"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
-            </svg>
-            <p>Num like</p>
-        </footer>
-    </article>
+    <?php endforeach; ?>
 </div>

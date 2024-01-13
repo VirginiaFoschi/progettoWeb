12f7880716fe5ce8326532c77c21d648cd57876b
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-13 15:27:57 +0100

    Modified method to access num of like

--- template/profilo-post.php
+++ template/profilo-post.php
@@ -42,7 +42,7 @@
                 viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
             </svg>
-            <p><?php echo $annuncio["NumLike"]["numLike"]?></p>
+            <p><?php echo $annuncio["NumLike"]?></p>
         </footer>
     </article>
     <?php endforeach; ?>

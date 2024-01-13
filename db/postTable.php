c6c8efabacdf7e7caef17a8f721dabd356494a12
unknown <margherita.balzoni@studio.unibo.it>
2024-01-13 12:20:13 +0100

    aggiunte funzioni per pubblicare i post

--- db/postTable.php
+++ db/postTable.php
@@ -31,5 +31,28 @@ class PostTable {
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC); 
     }
+
+    /*funzioni che permettono di pubblicare un post */
+
+    public function pubblicaAnnuncio($dataEvento, $luogo, $descrizione, $immagine, $usernameAutore ){
+        $stmt = $this->db->prepare("INSERT INTO EVENTO(ID_Evento, Data, Luogo, Descrizione, Immagine, Usurname_Autore) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('isssss', $data, $luogo, $descrizione, $immagine, $usernameAutore);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    }
+
+    public function pubblicaRecensione($autoreRecensione, $voto, $titolo, $autoreLibro, $immagine, $recensione){
+        $stmt = $this->db->prepare("INSERT INTO RECENSIONE(Autore_Recensione, Voto, Titolo_Libro, Autore_Libro, Immagine,Recensione) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('sissss', $autoreRecensione, $voto, $titolo, $autoreLibro, $immagine, $recensione);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    }
+
+    public function pubblicaLibro($titolo, $autore, $casaEditrice, $trama, $condizioni , $Immagine, $usernameAutore, $genere){
+        $stmt = $this->db->prepare("INSERT INTO LIBRO_POSTATO(ID_Libro, Titolo, Autore, Casa_Editrice, Trama, Condizioni, Immagine, Username_Autore, Nome_Genere) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('issssssss',$titolo, $autore, $casaEditrice, $trama, $condizioni , $Immagine, $usernameAutore, $genere);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    }
 }
 ?>

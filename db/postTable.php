c25f6982f8a0cde34a3cf87452e191505faf74fd
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-13 15:28:25 +0100

    Modified getRecensione method

--- db/postTable.php
+++ db/postTable.php
@@ -44,7 +44,25 @@ class PostTable
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC);
+        $recensioni = $result->fetch_all(MYSQLI_ASSOC);
+
+        $risultatoFinale = array();
+
+        foreach ($recensioni as $recensione) {
+
+            $stmt = $this->db->prepare("SELECT COUNT(*) AS numLike FROM interazione WHERE Autore_Recensione = ? AND Titolo_Libro = ? AND Autore_Libro = ?");
+            $stmt->bind_param('sss', $recensione["Autore_Recensione"], $recensione["Titolo_Libro"], $recensione["Autore_Libro"]);
+            $stmt->execute();
+            $result1 = $stmt->get_result();
+            $row = $result1->fetch_assoc();
+
+            $risultatoFinale[] = array(
+                'Recensione' => $recensione,
+                'NumLike' => $row['numLike']
+            );
+
+        }
+        return $risultatoFinale;
     }
 
     public function getPostLibroProfilo($username)

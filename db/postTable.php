417d57c9f560534481b7f6e8262766e1e9acb86c
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-13 23:27:02 +0100

    Modified Select element

--- db/postTable.php
+++ db/postTable.php
@@ -10,18 +10,19 @@ class PostTable
         $this->db = $db;
     }
 
-
     public function getAnnuncioProfilo($username)
     {
-        $stmt = $this->db->prepare("SELECT ID_Evento, dataEvento, luogo, nomeEvento, descrizione, immagine FROM evento WHERE Usurname_Autore = ?");
+        $stmt = $this->db->prepare("SELECT ID_Evento, dataEvento, luogo, nomeEvento, descrizione FROM evento WHERE Usurname_Autore = ?");
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();
         $annunci = $result->fetch_all(MYSQLI_ASSOC);
 
+
         $risultatoFinale = array();
 
         foreach ($annunci as $annuncio) {
+
 
             $stmt = $this->db->prepare("SELECT COUNT(*) AS numLike FROM interesse WHERE ID_Evento = ?");
             $stmt->bind_param('i', $annuncio["ID_Evento"]);
@@ -67,7 +68,7 @@ class PostTable
 
     public function getPostLibroProfilo($username)
     {
-        $stmt = $this->db->prepare("SELECT Titolo, Autore, Casa_Editrice, Trama, Condizioni, Immagine, Nome_Genere FROM libro_postato WHERE Usurname_Autore = ? ");
+        $stmt = $this->db->prepare("SELECT * FROM libro_postato WHERE Usurname_Autore = ? ");
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $result = $stmt->get_result();

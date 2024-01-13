7de3621a9fc81d4d8df5a0e404b71afaf305230f
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-13 15:09:52 +0100

    Fixed query

--- db/scambiTable.php
+++ db/scambiTable.php
@@ -30,14 +30,14 @@ class ScambiTable
 
             // Titolo, Immagine e Usurname_Utente del libro corrente
             $stmt = $this->db->prepare("SELECT Titolo, Immagine, Usurname_Autore FROM libro_postato WHERE ID_Libro = ?");
-            $stmt->bind_param('s', $id_libro['$ID_Libro']);
+            $stmt->bind_param('i', $id_libro["ID_Libro"]);
             $stmt->execute();
             $resultLibro = $stmt->get_result();
             $rowLibro = $resultLibro->fetch_assoc(); //Una riga del risultato
 
             // Esegui la query per trovare tutti i libri2 associati al libro corrente e la data di scadenza
             $stmt = $this->db->prepare("SELECT ID_Libro2, Data_Fine FROM scambio WHERE ID_Libro1 = ?");
-            $stmt->bind_param('s', $id_libro['$ID_Libro']);
+            $stmt->bind_param('i', $id_libro["ID_Libro"]);
             $stmt->execute();
             $result = $stmt->get_result();
 
@@ -46,7 +46,7 @@ class ScambiTable
 
                 // Titolo, Immagine e Usurname_Utente del libro corrente
                 $stmt = $this->db->prepare("SELECT Titolo, Immagine, Usurname_Autore FROM libro_postato WHERE ID_Libro = ?");
-                $stmt->bind_param('s', $libro2);
+                $stmt->bind_param('i', $libro2);
                 $stmt->execute();
                 $resultLibro2 = $stmt->get_result();
                 $row2 = $resultLibro2->fetch_assoc();
@@ -54,7 +54,7 @@ class ScambiTable
 
                 if ($row2) {
                     $resultPerLibro[] = array(
-                        'Libro1' => array('ID_Libro1' => $id_libro, 'DettagliLibro1' => $rowLibro),
+                        'Libro1' => array('ID_Libro1' => $id_libro["ID_Libro"], 'DettagliLibro1' => $rowLibro),
                         'Libro2' => array('ID_Libro2' => $libro2, 'DettagliLibro2' => $row2),
                         'Data_Fine' => $row['Data_Fine']
                     );
@@ -66,7 +66,7 @@ class ScambiTable
 
             // Esegui la query per trovare tutti i libri2 associati al libro corrente e la data di scadenza
             $stmt = $this->db->prepare("SELECT ID_Libro1, Data_Fine FROM scambio WHERE ID_Libro2 = ?");
-            $stmt->bind_param('s', $id_libro['$ID_Libro']);
+            $stmt->bind_param('i', $id_libro["ID_Libro"]);
             $stmt->execute();
             $resultLibro1 = $stmt->get_result();
 
@@ -75,7 +75,7 @@ class ScambiTable
 
                 // Titolo, Immagine e Usurname_Utente del libro corrente
                 $stmt = $this->db->prepare("SELECT Titolo, Immagine, Usurname_Autore FROM libro_postato WHERE ID_Libro = ?");
-                $stmt->bind_param('s', $libro1);
+                $stmt->bind_param('i', $libro1);
                 $stmt->execute();
                 $resultLibroUno = $stmt->get_result();
                 $row2 = $resultLibroUno->fetch_assoc();
@@ -83,7 +83,7 @@ class ScambiTable
 
                 if ($row2) {
                     $resultPerLibro[] = array(
-                        'Libro1' => array('ID_Libro1' => $id_libro['$ID_Libro'], 'DettagliLibro1' => $rowLibro),
+                        'Libro1' => array('ID_Libro1' => $id_libro["ID_Libro"], 'DettagliLibro1' => $rowLibro),
                         'Libro2' => array('ID_Libro2' => $libro1, 'DettagliLibro2' => $row2),
                         'Data_Fine' => $row['Data_Fine']
                     );

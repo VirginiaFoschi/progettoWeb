30faf9551545a392f84107d19111803aca2c888f
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-13 17:12:05 +0100

    add upload folder that contains images and create folder utils that contains a file with useful function such as function updateImage and add this folders to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 require_once("db/database.php");
+require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "elaboratoweb", 3307); //per la creazione del database
 define("UPLOAD_DIR", "./upload/")
 ?>

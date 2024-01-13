77b82fe77a5dcea79f7638f3c75d1b2ee7a7a5c4
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-13 22:38:44 +0100

    use base base-home for bacheca and search

--- search.php
+++ search.php
@@ -1,9 +1,19 @@
 <?php
     require_once("bootstrap.php");
 
-    $templateparams["nome"] = "search.php";
-    $templateparams["css"] = array("css/search.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
-    $templateparams["js"] = array("js/search.js");
+    $paginaCorrente="search";
+    $text="";
+    $templateparams["nome"] = "base-search.php";
+    $templateparams["css"] = array("search.css"); //, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+    $templateparams["js"] = array("search.js");
+    $templateparams["users"] = $dbh->getUsersTable()->getUserByInitialLetters($text);
+    $templateparams["generi"] = $dbh->getPreferencesTable()->getGenres();
+    $templateparams["follows"] = array_column($dbh->getFollowsTable()->getFollows($_SESSION["username"]), "username_seguito");
 
-    require("template/base.php");
+    if(isset($_POST["search"])) {
+        $text=$_POST["search"];
+        $templateparams["users"] = $dbh->getUsersTable()->getUserByInitialLetters($text);
+    }
+
+    require("template/base-home.php");
 ?>

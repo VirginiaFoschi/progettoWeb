5ac35f6b9ce50b8fcf281d180be3a54b466cb58e
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-14 18:55:21 +0100

    css and js for likes and follow common

---
+++ js/likes-follow.js
@@ -0,0 +1,23 @@
+let el=document.querySelectorAll(".heart-icon");
+for (let i = 0; i < el.length; i++) {
+    el[i].addEventListener('click', function(){
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            this.classList.add('active');
+        }
+    });
+};
+
+let buttons= document.getElementsByClassName('follow');
+for(let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        let value = this.value;
+        if(value == "Segui") {
+            this.value='Segui già';
+        } else {
+            this.value = 'Segui';
+        }
+        
+    });
+}

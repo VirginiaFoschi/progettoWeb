c9436722a823257d1f6d59c33f7d22e01af1045b
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-14 01:04:56 +0100

    Add recensione to profilo-post

--- profilo-post.php
+++ profilo-post.php
@@ -6,6 +6,7 @@ $paginaCorrente = 'profilo';
 $postCorrente = 'post';
 $templateparams["nome-articolo"] = "profilo-post.php";
 $templateparams["annuncio"] = $dbh->getPostTable()->getAnnuncioProfilo("chiaCasti6");
+$templateparams["recensione"] = $dbh->getPostTable()->getRecensioneProfilo("chiaCasti6");
 
 require("profilo.php");
 ?>

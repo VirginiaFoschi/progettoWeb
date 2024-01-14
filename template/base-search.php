69bf0f2d1166a0fb24a04c08b20ab3e3572b5c6c
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-14 19:13:49 +0100

    update search with all functionalities except propose

--- template/base-search.php
+++ template/base-search.php
@@ -1,32 +1,23 @@
-<div class="container-fluid p-0 overflow-hidden">  
-<div class="row mx-3 my-2">
-    <div class="col-12"> 
-        <header>
-        <form action="#" method="POST">
-            <button id="btnsearch" type="submit"><strong class="fa fa-search"></strong></button>
-            <input id="cerca" type="text" placeholder="Search.." name="search" title="searchbtn">
-            <button id="btnannulla" type="button">Annulla</button>
-        </form>
-        </header>
-    </div>
-</div>
-<div class="row justify-content-center mb-2">
-    <div class="col-12"> 
-        <ul class="nav justify-content-center bg-light" id="options">
-            <li class="nav-item">
-                <a class="nav-link <?php echo ($sottopagina == 'all') ? 'active3' : ''; ?>" aria-current="page" href="search-all.php" onclick="cambiaTab3(this)">All</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?php echo ($sottopagina == 'users') ? 'active3' : ''; ?>" href="search-users.php" onclick="cambiaTab3(this)">Users</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?php echo ($sottopagina == 'posts') ? 'active' : ''; ?>" href="search-posts.php" onclick="cambiaTab3(this)">Posts</a>
-            </li>
-        </ul>
-    </div>
-</div>
+<header>
+    <form action="#" method="POST">
+        <button id="btnsearch" type="submit"><strong class="fa fa-search"></strong></button>
+        <input id="cerca" class="<?php if($_SESSION["text"] != ""): echo 'restore'; endif; ?>" type="text" placeholder="<?php echo $_SESSION["text"] == "" ? 'Search..' :  $_SESSION["text"]; ?>" name="search" title="searchbtn">
+        <button id="btnannulla" class="<?php if($_SESSION["text"] != ""): echo 'restore'; endif; ?>" type="submit">Annulla</button>
+    </form>
+</header>
+<ul class="nav justify-content-center bg-light" id="options">
+    <li class="nav-item">
+        <a class="nav-link <?php echo ($sottopagina == 'all') ? 'active3' : ''; ?>" aria-current="page" href="search-all.php" onclick="cambiaTab3(this)">All</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php echo ($sottopagina == 'users') ? 'active3' : ''; ?>" href="search-users.php" onclick="cambiaTab3(this)">Users</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php echo ($sottopagina == 'posts') ? 'active3' : ''; ?>" href="search-posts.php" onclick="cambiaTab3(this)">Posts</a>
+    </li>
+</ul>
 <div class="container-fluid p-0 overflow-hidden content">
-<?php
-            require($templateparams["nome1"]);
-        ?>
+    <?php
+        require($templateparams["nome1"]);
+    ?>
 </div>

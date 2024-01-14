c085ceed1af9406c66c986681d9622aad0fd9cf7
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-14 22:52:32 +0100

    add line to set genre

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,7 @@ session_start();
 require_once("db/database.php");
 require_once("utils/functions.php");
 registerLastResearch(isset($_SESSION["text"]) ? $_SESSION["text"] : "");
+registerGenre(isset($_SESSION["genere"]) ? $_SESSION["genere"] : "");
 $dbh = new DatabaseHelper("localhost", "root", "", "elaborato_web", 3307); //per la creazione del database
 define("UPLOAD_DIR", "./upload/")
 ?>

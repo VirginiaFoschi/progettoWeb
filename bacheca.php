e71f4b3c284aa965ae2fe264975f69599d8a2fc7
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-14 18:56:18 +0100

    order files on pubblication date

--- bacheca.php
+++ bacheca.php
@@ -5,11 +5,19 @@
     $templateparams["eventi"] = $dbh->getEventsTable()->getEvents();
     $templateparams["recensioni"] = $dbh->getReviewsTable()->getReviews();
     $templateparams["nome"] = "bacheca.php";
-    $templateparams["css"] = array("bacheca.css");
-    $templateparams["js"] = array("bacheca.js");
+    $templateparams["css"] = array("bacheca.css", "likes-follow.css");
+    $templateparams["js"] = array("likes-follow.js");
     $templateparams["follows"] = array_column($dbh->getFollowsTable()->getFollows($_SESSION["username"]), "username_seguito");
     $templateparams["likes-reviews"] = $dbh->getInteractionsTable()->getUserLikes($_SESSION["username"]);
     $templateparams["likes-events"] = array_column($dbh->getInterestsTable()->getUserLikes($_SESSION["username"]), "id_evento");
+    $templateparams["posts"] = array_merge_recursive($templateparams["eventi"], $templateparams["recensioni"]);
+    
+    usort($templateparams["posts"], function($a, $b) {
+        $dataA = strtotime($a['dataPubblicazione']);
+        $dataB = strtotime($b['dataPubblicazione']);
+    
+        return $dataA - $dataB;
+    });
 
     require("template/base-home.php");
 ?>

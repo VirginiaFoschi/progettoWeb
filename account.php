2834736fe9abd0128fad921d99c0639bac973af1
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-14 18:15:57 +0100

    fixed type error

--- account.php
+++ account.php
@@ -4,7 +4,7 @@ require_once("bootstrap.php");
 $templateparams["nome"] = "account.php";
 $templateparams["js"] = array("profile-post.js", "bacheca.js");
 $templateparams["css"] = array("profilo.css");
-$templateparams["follows"] = array_column($dbh->getFollowsTable()->getFollows(/*$_SESSION["username"] */"chiaCasti6"), "usurname_seguito");
+$templateparams["follows"] = array_column($dbh->getFollowsTable()->getFollows(/*$_SESSION["username"] */"chiaCasti6"), "username_seguito");
 
 require("template/base-home.php");
 

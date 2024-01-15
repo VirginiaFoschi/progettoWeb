022fa2d3ccc8ee70799a38af6002a288ec8d09c8
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-15 22:19:24 +0100

    Ordered post for account-post.php

--- account-post.php
+++ account-post.php
@@ -19,5 +19,15 @@ $templateparams["nome-profilo"] = $idAccount;
 $templateparams["likes-reviews"] = $dbh->getInteractionsTable()->getUserLikes($_SESSION["username"]);
 $templateparams["likes-events"] = array_column($dbh->getInterestsTable()->getUserLikes($_SESSION["username"]), "id_evento");
 
+$templateparams["posts"] = array_merge($templateparams["annuncio"], $templateparams["recensione"]);
+
+usort($templateparams["posts"], function($a, $b){
+    $dataA = isset($a['Evento']) ? $a['Evento']['DataPubblicazione'] : $a['Recensione']['DataPubblicazione'];
+    $dataB = isset($b['Evento']) ? $b['Evento']['DataPubblicazione'] : $b['Recensione']['DataPubblicazione'];
+
+    return strtotime($dataB) - strtotime($dataA);
+});
+
+
 require("account.php");
 ?>

9b41240098abc372314f0994aae43b57e1781e02
unknown <margherita.balzoni@studio.unibo.it>
2024-01-16 00:38:32 +0100

    correzioni

--- notifications.php
+++ notifications.php
@@ -1,9 +1,13 @@
 <?php
     require_once("bootstrap.php");
+    $idAccount = -1;
+if(isset($_GET["id"])){
+    $idAccount = $_GET["id"];
+}
     $paginaCorrente = 'notifica';
-    $templateparams["notifiche"] = $dbh->getNotificationsTable()->getNotifications($_SESSION["username"]);
+    $templateparams["notifiche"] = $dbh->getNotificationsTable()->getNotifications("luigi_bianchi");
     $templateparams["nome"] = "notifications.php";
     $templateparams["css"] = array("notifications.css");
-   // $templateparams["js"] = array("notifications.js");
-    require("template/base-home.php");
+    $templateparams["js"] = array("notification.js");
+    require("template/base-notifiche.php");
 ?>

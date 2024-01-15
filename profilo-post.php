6a397994a8f2694e553c938373ca4e589abbd27c
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-15 22:09:56 +0100

    Ordered post on profilo-post.php

--- profilo-post.php
+++ profilo-post.php
@@ -12,6 +12,14 @@ $templateparams["nome-profilo"] = $username;
 
 $templateparams["likes-reviews"] = $dbh->getInteractionsTable()->getUserLikes($_SESSION["username"]);
 $templateparams["likes-events"] = array_column($dbh->getInterestsTable()->getUserLikes($_SESSION["username"]), "id_evento");
+$templateparams["posts"] = array_merge($templateparams["annuncio"], $templateparams["recensione"]);
+
+usort($templateparams["posts"], function($a, $b){
+    $dataA = isset($a['Evento']) ? $a['Evento']['DataPubblicazione'] : $a['Recensione']['DataPubblicazione'];
+    $dataB = isset($b['Evento']) ? $b['Evento']['DataPubblicazione'] : $b['Recensione']['DataPubblicazione'];
+
+    return strtotime($dataB) - strtotime($dataA);
+});
 
 require("profilo.php");
 ?>

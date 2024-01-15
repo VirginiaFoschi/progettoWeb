8bcf7f60fbb5fe1618d54baa5c622230279a44a7
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-15 16:39:52 +0100

    translate in italian

--- template/base-search.php
+++ template/base-search.php
@@ -1,19 +1,19 @@
 <header>
     <form action="#" method="POST">
         <button id="btnsearch" type="submit"><strong class="fa fa-search"></strong></button>
-        <input id="cerca" class="<?php if($_SESSION["text"] != ""): echo 'restore'; endif; ?>" type="text" placeholder="<?php echo $_SESSION["text"] == "" ? 'Search..' :  $_SESSION["text"]; ?>" name="search" title="searchbtn">
+        <input id="cerca" class="<?php if($_SESSION["text"] != ""): echo 'restore'; endif; ?>" type="text" placeholder="<?php echo $_SESSION["text"] == "" ? 'Cerca..' :  $_SESSION["text"]; ?>" name="search" title="searchbtn">
         <button id="btnannulla" class="<?php if($_SESSION["text"] != ""): echo 'restore'; endif; ?>" type="submit">Annulla</button>
     </form>
 </header>
 <ul class="nav justify-content-center bg-light" id="options">
     <li class="nav-item">
-        <a class="nav-link <?php echo ($sottopagina == 'all') ? 'active3' : ''; ?>" aria-current="page" href="search-all.php" onclick="cambiaTab3(this)">All</a>
+        <a class="nav-link <?php echo ($sottopagina == 'all') ? 'active3' : ''; ?>" aria-current="page" href="search-all.php" onclick="cambiaTab3(this)">Tutti</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?php echo ($sottopagina == 'users') ? 'active3' : ''; ?>" href="search-users.php" onclick="cambiaTab3(this)">Users</a>
+        <a class="nav-link <?php echo ($sottopagina == 'posts') ? 'active3' : ''; ?>" href="search-posts.php" onclick="cambiaTab3(this)">Post</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?php echo ($sottopagina == 'posts') ? 'active3' : ''; ?>" href="search-posts.php" onclick="cambiaTab3(this)">Posts</a>
+        <a class="nav-link <?php echo ($sottopagina == 'users') ? 'active3' : ''; ?>" href="search-users.php" onclick="cambiaTab3(this)">Persone</a>
     </li>
 </ul>
 <div class="container-fluid p-0 overflow-hidden content">

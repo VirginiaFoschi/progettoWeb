38b591c14cf29c78d0eab8013dc70f819af255a8
unknown <margherita.balzoni@studio.unibo.it>
2024-01-16 19:55:02 +0100

    aggiunte chiamata alla funzione per inserire uno scambio

--- notifications.php
+++ notifications.php
@@ -1,13 +1,20 @@
 <?php
-    require_once("bootstrap.php");
-    $idAccount = -1;
-if(isset($_GET["id"])){
+require_once("bootstrap.php");
+$idAccount = -1;
+if (isset($_GET["id"])) {
     $idAccount = $_GET["id"];
 }
-    $paginaCorrente = 'notifica';
-    $templateparams["notifiche"] = $dbh->getNotificationsTable()->getNotifications("luigi_bianchi");
-    $templateparams["nome"] = "notifications.php";
-    $templateparams["css"] = array("notifications.css");
-    $templateparams["js"] = array("notification.js");
-    require("template/base-notifiche.php");
-?>
+$paginaCorrente = 'notifica';
+$templateparams["notifiche"] = $dbh->getNotificationsTable()->getNotifications("luigi_bianchi");
+$templateparams["interesse"] = $dbh->getNotificationsTable()->getInterests("luigi_bianchi");
+$templateparams["interazione"] = $dbh->getNotificationsTable()->getInterests("luigi_bianchi");
+$templateparams["commenti"] = $dbh->getNotificationsTable()->getComments("luigi_bianchi");
+$templateparams["nome"] = "notifications.php";
+$templateparams["css"] = array("notifications.css");
+$templateparams["js"] = array("notification.js", "selelctBook.js");
+
+if (isset($_POST["selected_book"]) && isset($_POST["selected_book2"])) {
+    $dbh->getScambioTable()->setScambio($_POST["selected_book"], $_POST["selected_book2"]);
+}
+
+require("template/base-notifiche.php");

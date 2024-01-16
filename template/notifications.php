619b9b666763feae2fb9fa487fbfd081554f1e93
unknown <margherita.balzoni@studio.unibo.it>
2024-01-16 19:54:05 +0100

    aggiunti commenti e like alle notifiche

--- template/notifications.php
+++ template/notifications.php
@@ -1,61 +1,128 @@
 <header>
-<a class="btn btn-primary m-3"  id="Back" onClick="" role="button">Back</a>
+  <a class="btn btn-primary m-3" id="Back" onClick="" role="button">Back</a>
 </header>
 <div class="col">
-<?php foreach($templateparams["notifiche"] as $notifica): ?>
-    <?php if(!array_key_exists('id_notifica', $notifica)): ?>
-       <?php $username = $notifica["Username_Int"];
-            $books =$dbh->getPostTable()->getPostLibroProfilo($username); ?>
-            <div class="row-justify-content-center">
-                <article class="article bg-body border mb-3">
-                    <header class="px-3  mt-3 mb-3">
-                        <img src="<?php echo '<img src="' .UPLOAD_DIR.$notifica["userProfileImage"] ; ?>" alt="" class="img_profile">
-                        <a href="<?php  echo "account-notifiche.php";?>?id=<?php echo  $notifica["Username_Int"];?>"><?php echo $notifica["Username_Int"]; ?></a>
-                    </header>
-                    <section class="px-3 mb-4">
-                        <h4>Vuole proporti uno scambio con</h4> 
-                    </div>
-                    </section>
-                    <footer class="text-end my-3 px-3">
-                        <a class="btn btn-secondary m-3 rifiuta" href="#" role="button" >
-                            Rifiuta</a>
-                        <a class="btn btn-primary m-3 accetta" href="#" role="button"  data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            Scambia</a>
-                    </footer>
-                </article>
-               
-            </div>
-         
-        </div>
-        <?php endif; ?>
-<?php endforeach; ?>
+  <?php foreach ($templateparams["notifiche"] as $notifica) : ?>
+    <?php if (!array_key_exists('id_notifica', $notifica)) : ?>
+      <?php $username = $notifica["Username_Int"]; ?>
+      <div class="row-justify-content-center">
+        <article class="article bg-body border mb-3">
+          <header class="px-3  mt-3 mb-3">
+            <img src="<?php echo '<img src="' . UPLOAD_DIR . $notifica["userProfileImage"]; ?>" alt="" class="img_profile">
+            <a href="<?php echo "account-post.php"; ?>?id=<?php echo  $notifica["Username_Int"]; ?>"><?php echo $notifica["Username_Int"]; ?></a>
+            <?php $idBook = $notifica["ID_Libro"] ?>
+          </header>
+          <section class="px-3 mb-4">
+            <h4>Vuole proporti uno scambio con <?php echo $notifica["bookTitle"]; ?></h4>
+      </div>
+      </section>
+      <footer class="text-end my-3 px-3">
+        <a class="btn btn-secondary m-3 rifiuta" href="#" role="button">
+          Rifiuta</a>
+        <a class="btn btn-primary m-3 accetta" href="#" role="button" data-bs-toggle="modal" onclick="setIdBook(<?php echo $notifica['ID_Libro']; ?>)" data-bs-target="#exampleModal">
+          Scambia</a>
+      </footer>
+      </article>
+
+</div>
+
+</div>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Libri di nome.utente</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                <?php foreach ($books as $postLibro): ?>
-                    <article class="article-annuncio bg-body border mb-3">
-                        <header class="px-3  mt-3 mb-3">
-                        <img src="<?php echo UPLOAD_DIR . $postLibro["Immagine"] ?>" alt="Copertina libro" class="image" />
-                        <h3><?php echo $postLibro["Titolo"] ?><h3>
-                        <h4><?php echo $postLibro["Autore"] ?></h4>
-                        </header>
-                          <section class="px-3 mb-4">
-                         <p><?php echo $postLibro["Trama"] ?></p> 
-                              <input type="button" value="Seleziona" name="seleziona-libro"   class="seleziona-libro">
-                    </article>
-                     <?php endforeach; ?>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                  <button type="button" class="btn btn-primary conferma" data-bs-dismiss="modal">Conferma</button>
-                </div>
-              </div>
-            </div>
-          </div>
-         
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Libri di <?php echo $username; ?></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="#" method="Post">
+          <input type="hidden" id="select" name="selected_book" value="book_id">
+          <input type="hidden" id="select2" name="selected_book2" value="<?php echo  $notifica["ID_Libro"]; ?>">
+          <?php $books = $dbh->getPostTable()->getPostLibroProfilo($username); // aggiorna la variabile $books con i libri dell'autore della notifica ?>
+          <?php foreach ($books as $postLibro) : ?>
+            <article class="article-annuncio bg-body border mb-3">
+              <header class="px-3  mt-3 mb-3">
+                <img src="<?php echo UPLOAD_DIR . $postLibro["Immagine"] ?>" alt="Copertina libro" class="image" />
+                <h3><?php echo $postLibro["Titolo"] ?><h3>
+                    <h4><?php echo $postLibro["Autore"] ?></h4>
+              </header>
+              <section class="px-3 mb-4">
+                <p><?php echo $postLibro["Trama"] ?></p>
+                <input type="button" value="Seleziona" name="seleziona-libro" class="seleziona-libro" onclick="setSelectedBook(<?php echo $postLibro['ID_Libro']; ?>)">
+            </article>
+          <?php endforeach; ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+        <button type="button" class="btn btn-primary conferma" data-bs-dismiss="modal" onclick="sendForm()">Conferma</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+<?php endforeach; ?>
+
+
+<?php foreach ($templateparams["interesse"] as $interesse) : ?>
+  <?php if (!array_key_exists('id_interesse', $interesse)) : ?>
+    <div class="row-justify-content-center">
+      <article class="article bg-body border mb-3">
+        <header class="px-3  mt-3 mb-3">
+          <img src="<?php echo '<img src="' . UPLOAD_DIR . $interesse["userProfileImage"]; ?>" alt="" class="img_profile">
+          <a href="<?php echo "account-post.php"; ?>?id=<?php echo  $interesse["Username_Int"]; ?>"><?php echo $interesse["Username_Int"]; ?></a>
+        </header>
+        <section class="px-3 mb-4">
+          <h4>Ha messo like al tuo post</h4>
+    </div>
+    </section>
+    </article>
+
+    </div>
+
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>
+
+<?php foreach ($templateparams["interazione"] as $interazione) : ?>
+  <?php if (!array_key_exists('id_interazione', $interazione)) : ?>
+    <div class="row-justify-content-center">
+      <article class="article bg-body border mb-3">
+        <header class="px-3  mt-3 mb-3">
+          <img src="<?php echo '<img src="' . UPLOAD_DIR . $interazione["userProfileImage"]; ?>" alt="" class="img_profile">
+          <a href="<?php echo "account-post.php"; ?>?id=<?php echo  $interazione["Username_Int"]; ?>"><?php echo $interazione["Username_Int"]; ?></a>
+        </header>
+        <section class="px-3 mb-4">
+          <h4>Ha messo like alla tua recensione</h4>
+    </div>
+    </section>
+    </article>
+
+    </div>
+
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>
+
+<?php foreach ($templateparams["commenti"] as $commento) : ?>
+  <?php if (!array_key_exists('id_commenti', $commento)) : ?>
+    <div class="row-justify-content-center">
+      <article class="article bg-body border mb-3">
+        <header class="px-3  mt-3 mb-3">
+          <img src="<?php echo '<img src="' . UPLOAD_DIR . $commento["userProfileImage"]; ?>" alt="" class="img_profile">
+          <a href="<?php echo "account-post.php"; ?>?id=<?php echo  $commento["Autore_Commento"]; ?>"><?php echo $commento["Autore_Commento"]; ?></a>
+        </header>
+        <section class="px-3 mb-4">
+          <h4>Ha commentato il tuo post</h4>
+          <p><?php echo $commento["Testo_Commento"]; ?>
+          <p>
+    </div>
+    </section>
+    </article>
+
+    </div>
+
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>

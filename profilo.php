4befd228aa32a15773637f28c598eafa45643632
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-17 15:46:50 +0100

    Add ..altro to profilo

--- profilo.php
+++ profilo.php
@@ -2,7 +2,7 @@
 
 require_once("bootstrap.php");
 $templateparams["nome"] = "profilo.php";
-$templateparams["js"] = array("profile-post.js", "likes-follow.js");
+$templateparams["js"] = array("profile-post.js", "likes-follow.js", "search.js");
 $templateparams["css"] = array("profilo.css", "likes-follow.css");
 $templateparams["follower"] = $dbh->getUsersTable()->getFollower($username);
 $templateparams["follow"] = $dbh->getUsersTable()->getFollow($username);

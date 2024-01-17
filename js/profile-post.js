4a45f4e6827f30eb54a4461b632309469aecddb7
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-17 15:18:42 +0100

    Change id

--- js/profile-post.js
+++ js/profile-post.js
@@ -17,7 +17,8 @@ function scaricaEtichetta($libro1, $libro2) {
 let comment = document.getElementsByClassName("comment-icon");
 for (let i = 0; i < comment.length; i++) {
   comment[i].addEventListener('click', function () {
-    let form = document.getElementById(this.id);
+    let id = this.getAttribute('data-target');
+    let form = document.getElementById(id);
     form.style.display = 'block';
   });
 }

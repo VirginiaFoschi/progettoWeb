6ce34fa55b0741197c841b823c209e61ed3a9acd
Virginia Foschi <virginia.foschi02@gmail.com>
2024-01-18 18:43:27 +0100

    css and delete unused files

--- review.php
+++ review.php
@@ -3,9 +3,9 @@ $paginaCorrente='addBook';
 require_once("bootstrap.php");
 $templateparams["nome"] = "review.php";
 $templateparams["css"] = array("post.css");
-$templateparams["js"] = array("changeImg.js", "addPost.js", "stars.js");
+$templateparams["js"] = array("changeImg.js", "stars.js");
 $templateparams["generi"] = $dbh->getGenresTable()->getGenres();
-$image = "upload/InsereisciImg.jpeg";
+$image = "InserisciImg.jpeg";
 
 if (
     isset($_POST["titolo"]) && isset($_POST["autoreLibro"]) && isset($_POST["voto"])
@@ -15,7 +15,7 @@ if (
     if (isset($_FILES["image"]) && $_FILES["image"]["size"] > 0) {
         list($result, $msg) = uploadImage(UPLOAD_DIR, $_FILES["image"]);
         if ($result != 0) {
-            $image = $msg;
+            $image = $_FILES["image"]["name"];
 
             $dbh->getPostTable()->pubblicaRecensione($_SESSION["username"], $_POST["voto"], $_POST["titolo"], $_POST["autoreLibro"], $image, $_POST["recensione"]);
 

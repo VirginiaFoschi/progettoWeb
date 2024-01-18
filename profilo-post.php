88361da77bb7b2d718157f0d703bcb2fc3a6d16d
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-18 09:21:37 +0100

    Fixed commenti in post

--- profilo-post.php
+++ profilo-post.php
@@ -13,14 +13,6 @@ $templateparams["nome-profilo"] = $username;
 $templateparams["likes-reviews"] = $dbh->getInteractionsTable()->getUserLikes($_SESSION["username"]);
 $templateparams["likes-events"] = array_column($dbh->getInterestsTable()->getUserLikes($_SESSION["username"]), "id_evento");
 $templateparams["posts"] = array_merge($templateparams["annuncio"], $templateparams["recensione"]);
-$templateparams["commenti"] = $dbh->getPostTable()->getComment();
-
-usort($templateparams["commenti"], function ($a, $b) {
-    $dataA = strtotime($a['DataPubblicazione']);
-    $dataB = strtotime($b['DataPubblicazione']);
-
-    return $dataA - $dataB;
-});
 
 usort($templateparams["posts"], function ($a, $b) {
     $dataA = isset($a['Evento']) ? $a['Evento']['DataPubblicazione'] : $a['Recensione']['DataPubblicazione'];
@@ -29,20 +21,25 @@ usort($templateparams["posts"], function ($a, $b) {
     return strtotime($dataB) - strtotime($dataA);
 });
 
+function getComments($id_evento) {
+    global $dbh;
+    $commenti=$dbh->getPostTable()->getCommentEvent($id_evento);
+    usort($commenti, function ($a, $b) {
+        $dataA = strtotime($a['DataPubblicazione']);
+        $dataB = strtotime($b['DataPubblicazione']);
+
+        return $dataA - $dataB;
+    });
+    return $commenti;
+}
+
 if (isset($_POST["commento"]) && isset($_POST["id_evento"])) {
     $commento = $_POST["commento"];
     $id_evento = $_POST["id_evento"];
     $autore_commento = $_SESSION["username"];
     $dbh->getPostTable()->addComment($commento, $id_evento, $autore_commento);
     header("Location: profilo-post.php");
-    $templateparams["commenti"] = $dbh->getPostTable()->getComment();
-
-    usort($templateparams["commenti"], function ($a, $b) {
-        $dataA = strtotime($a['DataPubblicazione']);
-        $dataB = strtotime($b['DataPubblicazione']);
-
-        return $dataA - $dataB;
-    });
+    
 }
 
 require("profilo.php");

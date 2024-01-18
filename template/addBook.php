be8c9d2fb9926dd3afbc5c2050870725e77ac27b
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-01-18 19:23:53 +0100

    Fixed css post

--- template/addBook.php
+++ template/addBook.php
@@ -21,9 +21,11 @@
                 <img src="upload/InserisciImg.jpeg" alt="" class="img_profile" id="copertina">
                 <input type="file" id="fileInputBook" accept="image/*" style="display: none;" name="image">
                 <a class="nav-link" href="#" onclick="addBook()">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" id="addImg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                        class="bi bi-plus-circle" viewBox="0 0 16 16" id="addImg">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                     </svg>
                 </a>
             </div>
@@ -55,7 +57,8 @@
                     <li>
 
                         <label for="floatingSelectCondizioni">Condizioni</label>
-                        <select class="form-select " id="floatingSelectCondizioni" aria-label="Floating label select example" name="condizioni">
+                        <select class="form-select " id="floatingSelectCondizioni"
+                            aria-label="Floating label select example" name="condizioni">
                             <option selected>Ottime</option>
                             <option value="1">Buone</option>
                             <option value="2">Un po' rovinato</option>
@@ -66,9 +69,12 @@
                     <li>
 
                         <label for="floatingSelectGenere">Genere</label>
-                        <select class="form-select " id="floatingSelectGenere" aria-label="Floating label select example" name="genere">
-                            <?php foreach ($templateparams["generi"] as $genere) : ?>
-                                <option value="<?php echo $genere["nome"]; ?>"><?php echo $genere["nome"] ?></option>
+                        <select class="form-select " id="floatingSelectGenere"
+                            aria-label="Floating label select example" name="genere">
+                            <?php foreach ($templateparams["generi"] as $genere): ?>
+                                <option value="<?php echo $genere["nome"]; ?>">
+                                    <?php echo $genere["nome"] ?>
+                                </option>
                             <?php endforeach; ?>
                         </select>
 
@@ -78,18 +84,19 @@
                     <li>
 
                         <label for="floatingTextarea">Trama</label>
-                        <textarea class="form-control " placeholder="Trama" id="floatingTextarea" name="trama"></textarea>
+                        <textarea class="form-control " placeholder="Trama" id="floatingTextarea"
+                            name="trama"></textarea>
 
                     </li>
                 </ul>
             </div>
             <footer>
-        <div class="mb-3 text-center">
+                <div class="mb-3 text-center">
 
-            <button id="invia" type="submit" class="btn" value="send">Invia</button>
+                    <button id="invia" type="submit" class="btn" value="send">Invia</button>
 
-        </div>
-    </footer>
+                </div>
+            </footer>
         </form>
     </section>
 </main>
